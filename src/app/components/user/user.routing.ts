d33327af59a71a5fff922859d5aa64afe21f0937
izhar <izharmalik4@gmail.com>
2019-05-04 18:01:17 +0530

    shared module/ blog component

--- src/app/components/user/user.routing.ts
+++ src/app/components/user/user.routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BlogComponent } from '../shared/blog/blog.component';
 
 
 export const UserRoutes: Routes = [
@@ -14,6 +15,16 @@ export const UserRoutes: Routes = [
           title: 'Dashboard ',
           urls: [
             { title: 'Dashboard', url: 'user/dashboard' },
+          ]
+        }
+      },
+      {
+        path: 'blog',
+        component: BlogComponent,
+        data: {
+          title: 'Blog ',
+          urls: [
+            { title: 'Blog', url: 'user/blog' },
           ]
         }
       }
